84192d9ee9594362f915e26a360b11d31fd64941
aviad-mauda <maooda@gmail.com>
2020-11-22 01:30:23 +0200

    delete story and chapter after submitting

--- submit/submit.js
+++ submit/submit.js
@@ -3,6 +3,10 @@ angular.module('submitModule', [])
     $scope.submit = () => { 
         $scope.user.gender = $rootScope.gendersRes.get($scope.user.gender);
         $scope.user.type = $rootScope.typesRes.get($scope.user.type);
+        if($scope.user.story == "") $scope.user.story = undefined;
+        if($scope.user.chapter == "") {
+            $scope.user.chapter= undefined;
+        }
         $http.post(serverDomain+"/addUser", $scope.user).then( async (res)=>{
             try{
                 await $rootScope.getAvailableChapters();
@@ -14,6 +18,8 @@ angular.module('submitModule', [])
             $scope.userRes.type = getByValue(res.data.user.type);
             delete $scope.user.name;
             delete $scope.user.parent;
+            delete $scope.user.type;
+            delete $scope.user.gender;
         }, (err)=>{
             console.log(err);
         });
